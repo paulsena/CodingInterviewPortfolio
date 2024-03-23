f3aab93d288e6570d46109ca1ec05ec1f91ea82c
Paul Senatillaka <826073+paulsena@users.noreply.github.com>
2024-03-22 21:17:38 -0400

    Time: 0 ms (100.00%) | Memory: 44.7 MB (14.89%) - LeetSync

---
+++ LeetCode/110-balanced-binary-tree/balanced-binary-tree.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return checkHeight(root) != -1;
+    }
+
+    private int checkHeight(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftHeight = checkHeight(node.left);
+        if (leftHeight == -1) {
+            return -1; // Left subtree is not balanced
+        }
+
+        int rightHeight = checkHeight(node.right);
+        if (rightHeight == -1) {
+            return -1; // Right subtree is not balanced
+        }
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1; // Current node is not balanced
+        }
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
